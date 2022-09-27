bb347978877a4e496e9bf9c0813b69e1bfb21b04
AnoukPNL <112867115+AnoukPNL@users.noreply.github.com>
2022-09-27 16:00:16 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ console.log("hi");
 // ---- Nav ----
 // haal de element op van html met var
 var navbutton = document.querySelector("a:first-of-type");
-var nav = document.querySelector("nav:first-of-type");
+var nav = document.querySelector("header nav:first-of-type");
 
 // Luister als dit gebeurd voeg je toe
 // Toggle ipv add - want dan blijft hij aanwezig en kun je er vaker op drukken.

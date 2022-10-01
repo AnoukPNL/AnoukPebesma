a7237369d87ef6ee9b1c7ba97fed0a994dd4035a
AnoukPNL <112867115+AnoukPNL@users.noreply.github.com>
2022-10-01 16:23:49 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,20 @@ navbutton.addEventListener("click", function(){
     nav.classList.toggle("open")
 })
 
+
+// Thema switch
+var themaswitchelement = document.querySelector("footer button");
+
+function themaswitch(){
+    document.documentElement.classList.toggle("newtheme")
+}
+
+themaswitchelement.addEventListener("click",themaswitch);
+
+
+
+// Audio op de animatie bron: https://www.sitepoint.com/community/t/play-sound-on-hover-or-click-diffferent-audio-for-different-images/99391
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+}
